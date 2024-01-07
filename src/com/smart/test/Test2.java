2ef4313b5ad4a52426dfd5386935c019665bbeb9
SADIAANOWAR <kazi.sadia.anowar@g.bracu.ac.bd>
2024-01-06 20:57:02 -0500

    It's my first time using GitHub.

---
+++ src/com/smart/test/Test2.java
@@ -0,0 +1,12 @@
+package com.smart.test;
+
+public class Test2 {
+	
+	@org.testng.annotations.Test
+	public void loginTest() {
+		
+		System.out.println("We are learning Git and GitHub through Eclipse.");
+		System.out.println("We had our initial commit.");
+	}
+
+}
