eea2e988f288c32c783c190857297cbdd95488bf
SADIAANOWAR <kazi.sadia.anowar@g.bracu.ac.bd>
2024-01-06 21:08:44 -0500

    Using GitHub.

--- src/com/smart/test/Test2.java
+++ src/com/smart/test/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	
 	@org.testng.annotations.Test
 	public void loginTest() {
-		
+		//using GitHub 
 		System.out.println("We are learning Git and GitHub through Eclipse.");
 		System.out.println("We had our initial commit.");
 		System.out.println("We are learning automation engineering");
